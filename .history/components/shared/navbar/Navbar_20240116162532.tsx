26a1fbd960ed634525bcd7ee98c0e6ff20e4c90c
Moneeb919 <99467393+Moneeb919@users.noreply.github.com>
2024-01-16 16:50:21 +0530

    navbar

---
+++ .history/components/shared/navbar/Navbar_20240116162532.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none ">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          width={23}
+          height={23}
+          src="/assets/images/site-logo.svg"
+          alt="DevFlow"
+        />
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
